1668c0dddf13c1d5bdbe5acc9c7844df1314b45e
Mr.v <110378440+vamsy-Reddy@users.noreply.github.com>
2023-01-15 10:55:12 +0530

    Revert "updated to jsx and created a card around the application"

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = (props) => {
-    const classes = "card " + props.className;
-    return (
-        <div className={classes}>{props.children}</div>
-        // the value of children will always be the content between opening and closing tag of your custom component
-    )
-}
-
-export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
-import Card from "./Card";
-import ExpenseDate from "./ExpenseDate";
-
 const ExpenseItem = (props) => {
 
-   
   return (
-    <Card className="expense-item">
-      <div>
-        <ExpenseDate date={props.date} />
-      </div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">₹ {props.amount}</div>
-    </Card>
+    </div>
   );
 };
 
